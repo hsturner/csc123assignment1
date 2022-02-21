860743fc55b2e36f7d36954e581aec5eb4de943e
csc123 <csc123@debianVM>
2022-02-21 13:52:41 -0500

    first commit from laptop

---
+++ assignment1.c
@@ -0,0 +1,4 @@
+  /home/csc123/homework/assignment1:
+  total used in directory 8 available 5137028
+  drwxr-xr-x 2 csc123 csc123 4096 Feb 21 13:36 .
+  drwxr-xr-x 3 csc123 csc123 4096 Feb 21 13:36 ..
